6328aa99e93af4f4cf8efc524c4517d48273b36f
Keiran Kozlowski <keirankozlowski@gmail.com>
2021-07-08 13:36:25 -0400

    lecture commit 3

--- src/components/AnimalForm.test.js
+++ src/components/AnimalForm.test.js
@@ -19,7 +19,27 @@ describe("AnimalForm should submit a new animal array and display the array on t
 
         // Assert
         expect(speciesInput).toHaveValue('dog');
+    });
+    test("age input should accept text", () => {
+        // Arrange
+        render(<AnimalForm />);
 
-        // create the act/assert steps for the other two inputs and attempt to submit the form
+        // Act
+        const ageInput = screen.getByLabelText(/age/i);
+        userEvent.type(ageInput, "2");
+
+        // Assert
+        expect(ageInput).toHaveValue("2");
+    });
+    test("notes input should accept text", () => {
+        // Arrange
+        render(<AnimalForm />);
+
+        // Act
+        const notesInput = screen.getByLabelText(/notes/i);
+        userEvent.type(notesInput, "is very cute");
+
+        // Assert
+        expect(notesInput).toHaveValue("is very cute");
     });
 });
